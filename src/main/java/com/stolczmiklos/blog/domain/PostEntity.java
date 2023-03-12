01ce528649b7e4bdc561f2d230eab19239111347
Stolcz Miklós <stolcz.miklos@gmail.com>
2023-03-12 16:16:29 +0100

    created PostEntity with basic properties

---
+++ src/main/java/com/stolczmiklos/blog/domain/PostEntity.java
@@ -0,0 +1,79 @@
+package com.stolczmiklos.blog.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "text", columnDefinition = "text")
+    private String text;
+
+    @Column(name = "imageUrl")
+    private String imageUrl;
+
+    @Column(name = "videoUrl")
+    private String videoUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public PostEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
