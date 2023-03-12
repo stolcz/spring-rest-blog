5f441489c2f24458ace6adc91edd33679e6ccdd2
Stolcz Miklós <stolcz.miklos@gmail.com>
2023-03-12 15:56:11 +0100

    add webconfig & init; modify pom.xml

---
+++ src/main/java/com/stolczmiklos/blog/config/WebInitializer.java
@@ -0,0 +1,30 @@
+package com.stolczmiklos.blog.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringWebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{new CharacterEncodingFilter("UTF-8")};
+    }
+}
